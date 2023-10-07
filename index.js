0795bbcd02b62471bcd2de8b8e1cc3510b91cbb0
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-10-07 12:38:50 +0500

    classes

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 // OOP - Constructor functions
 
-function User(name, lastName, birthday) {
-  this.name = name;
-  this.lastName = lastName;
-  this.birthday = new Date(birthday);
+// function User(name, lastName, birthday) {
+//   this.name = name;
+//   this.lastName = lastName;
+//   this.birthday = new Date(birthday);
+// }
+
+// User.prototype.getBirthDayYear = function () {
+//   return this.birthday.getFullYear();
+// };
+
+// User.prototype.getFullName = function () {
+//   return `${this.name} ${this.lastName}`;
+// };
+
+// Classes
+class User {
+  constructor(name, lastName, birthday) {
+    this.name = name;
+    this.lastName = lastName;
+    this.birthday = new Date(birthday);
+  }
+
+  getBirthDayYear() {
+    return this.birthday.getFullYear();
+  }
+
+  getFullName() {
+    return `${this.name} ${this.lastName}`;
+  }
 }
-
-User.prototype.getBirthDayYear = function () {
-  return this.birthday.getFullYear();
-};
-
-User.prototype.getFullName = function () {
-  return `${this.name} ${this.lastName}`;
-};
 
 // Instance object
 const user1 = new User("John", "Doe", "1-2-1290");
